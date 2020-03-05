70a1d952fe67449d9ebb10e8cbb21f398d774492
SorayaP <sora.pch@gmail.com>
2020-03-05 16:49:58 -0300

    Vue file generated by Vuegg
    Trying out vuegg's editor file

---
+++ SateliteNoticias/frontend/src/tryouts/tagsPrincipales.vue
@@ -0,0 +1,119 @@
+<template>
+<div id="pAM8m-FH">
+  <div class="PHg9XG4P">
+    <select class="FHOhvA8y">
+      <option class="air9GrNnu">Option 1</option>
+    
+      <option class="51qmykxjh">Option 2</option>
+    
+      <option class="xmtBcdrKR">Option 3</option>
+    </select>
+  
+    <select class="Uo0uXUS6">
+      <option class="2wONXb-tu">Option 1</option>
+    
+      <option class="uA2XsZo4w">Option 2</option>
+    
+      <option class="QMVAegLZw">Option 3</option>
+    </select>
+  
+    <select class="3yLQFb0P">
+      <option class="1SCT4oYCO">Option 1</option>
+    
+      <option class="g0DKjigS9">Option 2</option>
+    
+      <option class="AKnPlp8Y0">Option 3</option>
+    </select>
+  
+    <span class="gwsMckEU">Text</span>
+  
+    <span class="rTQ0lIUn">Text</span>
+  
+    <select class="7pLawDEk">
+      <option class="KcicG0XzK">Option 1</option>
+    
+      <option class="OGhxUxEe8">Option 2</option>
+    
+      <option class="iUVEEoz58">Option 3</option>
+    </select>
+  
+    <span class="1qFLhJR6">Text</span>
+  
+    <span class="PAkc3S84">Text</span>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'tags-principales'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#pAM8m-FH {
+  --mdc-theme-primary: #673ab7;
+  --mdc-theme-secondary: #f44336;
+  --mdc-theme-background: #ffffff;
+  position: relative;
+  margin: auto;
+  background-color: #ffffff;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+}
+
+.gwsMckEU {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  position: absolute;
+  width: 42px;
+  height: 26px;
+  top: 33px;
+  left: 105px;
+}
+
+.rTQ0lIUn {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  position: absolute;
+  width: 46px;
+  height: 24px;
+  top: 32px;
+  left: 322px;
+}
+
+.1qFLhJR6 {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  position: absolute;
+  width: 48px;
+  height: 25px;
+  top: 33px;
+  left: 527px;
+}
+
+.PAkc3S84 {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  position: absolute;
+  width: 52px;
+  height: 25px;
+  top: 30px;
+  left: 730px;
+}
+
+.PHg9XG4P {
+  overflow: hidden;
+  border-color: rgba(0, 0, 0, 0.15);
+  border-style: solid;
+  border-width: 1px;
+  position: absolute;
+  width: 880px;
+  height: 99px;
+  top: 404px;
+  left: 191px;
+}
+
+</style>
