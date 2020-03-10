b260f11d461c46f8c809277a7b662bbd88ebd6f3
SorayaP <sora.pch@gmail.com>
2020-03-10 11:22:33 -0300

    HTML página principal
    HTML con los componentes de la página principal

---
+++ SateliteNoticias/frontend/src/tryouts/vistaPrincipal.vue
@@ -0,0 +1,177 @@
+<template>
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-10">
+					<h3 class="text-center">
+						h3. Lorem ipsum dolor sit amet.
+					</h3>
+				</div>
+				<div class="col-md-2">
+					<div class="row">
+						<div class="col-md-6">
+							 
+							<button type="button" class="btn btn-success">
+								Button
+							</button>
+						</div>
+						<div class="col-md-6">
+							 
+							<button type="button" class="btn btn-success">
+								Button
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3">
+					<div class="dropdown">
+						 
+						<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
+							Action
+						</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-7">
+					<div class="row">
+						<div class="col-md-4">
+							 
+							<button type="button" class="btn btn-success">
+								Button
+							</button>
+						</div>
+						<div class="col-md-4">
+						</div>
+						<div class="col-md-4">
+							 
+							<button type="button" class="btn btn-success">
+								Button
+							</button>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="jumbotron">
+										<h2>
+											Hello, world!
+										</h2>
+										<p>
+											This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
+										</p>
+										<p>
+											<a class="btn btn-primary btn-large" href="#">Learn more</a>
+										</p>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									<h3 class="text-right">
+										h3. Lorem ipsum dolor sit amet.
+									</h3>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-3">
+									<div class="dropdown">
+										 
+										<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
+											Action
+										</button>
+										<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+											 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-3">
+									<div class="dropdown">
+										 
+										<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
+											Action
+										</button>
+										<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+											 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-3">
+									<div class="dropdown">
+										 
+										<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
+											Action
+										</button>
+										<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+											 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-3">
+									<div class="dropdown">
+										 
+										<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
+											Action
+										</button>
+										<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+											 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-3">
+									<h3>
+										h3. Lorem ipsum dolor sit amet.
+									</h3>
+								</div>
+								<div class="col-md-3">
+									<div class="dropdown">
+										 
+										<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
+											Action
+										</button>
+										<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+											 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-3">
+								</div>
+								<div class="col-md-3">
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-6">
+								</div>
+								<div class="col-md-6">
+									 
+									<button type="button" class="btn btn-success">
+										Button
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</template>>
+<script>
+export default {
+    name: "vistaPrincipal"
+}
+</script>
