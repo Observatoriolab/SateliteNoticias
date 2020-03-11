46c398086d306b2ad6fc28d487a9875ab3fc2cd7
SorayaP <sora.pch@gmail.com>
2020-03-11 09:47:16 -0300

    Página principal
    modificación

--- SateliteNoticias/frontend/src/tryouts/vistaPrincipal.vue
+++ SateliteNoticias/frontend/src/tryouts/vistaPrincipal.vue
@@ -1,174 +1,188 @@
 <template>
-    <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="col-md-10">
+    <b-container class="bv-example-row">
+     <b-row>
+	        <b-col md="8">
 					<h3 class="text-center">
-						h3. Lorem ipsum dolor sit amet. asdasddasadsadasddsadsa
+						Satelite de Noticias 
 					</h3>
-				</div>
-				<div class="col-md-2">
-					<div class="row">
-						<div class="col-md-6">
+				</b-col>
+		    <b-col md="4">
+					<b-row>
+						<b-col md="6">
 							 
-							<button type="button" class="btn btn-success">
-								Button
-							</button>
-						</div>
-						<div class="col-md-6">
+							<b-button variant="success">Configuración</b-button>
+						</b-col>
+						<b-col md="6">
 							 
-							<button type="button" class="btn btn-success">
-								Button
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-3">
-					<div class="dropdown">
+							<b-button variant="success">Usuario</b-button>
+						</b-col>
+					</b-row>
+				</b-col>
+	</b-row>
+
+	<b-row>
+		<b-col md="12">
+		
+			<b-row>
+				<b-col md="3">
+					    <div class="dropdown">
 						 
-						<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
-							Action
-						</button>
-						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-							 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-7">
-					<div class="row">
-						<div class="col-md-4">
+						               <b-dropdown class="mx-1" right text="Categorias">
+                                           <b-dropdown-item>1 </b-dropdown-item>
+                                           <b-dropdown-item>2</b-dropdown-item>
+                                           <b-dropdown-item>3</b-dropdown-item>
+                                           <b-dropdown-item>4</b-dropdown-item>
+                                           <b-dropdown-item>5</b-dropdown-item>
+                                           <b-dropdown-item>6</b-dropdown-item>
+                                           <b-dropdown-item>7</b-dropdown-item>
+                                           <b-dropdown-item>8</b-dropdown-item>
+                                        </b-dropdown>
+						       <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							             <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+						       </div>
+					    </div>
+				</b-col> 
+            
+		            <b-col md="7">
+					         <b-row>
+						         <b-col md="4">
 							 
-							<button type="button" class="btn btn-success">
-								Button
-							</button>
-						</div>
-						<div class="col-md-4">
-						</div>
-						<div class="col-md-4">
+							            <b-button variant="success">Trending</b-button>
+						          </b-col>
+						        <b-col md="4">
+						        </b-col>
+						          <b-col md="4">
 							 
-							<button type="button" class="btn btn-success">
-								Button
-							</button>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-12">
-							<div class="row">
-								<div class="col-md-12">
-									<div class="jumbotron">
-										<h2>
-											Hello, world!
-										</h2>
-										<p>
-											This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
-										</p>
-										<p>
-											<a class="btn btn-primary btn-large" href="#">Learn more</a>
-										</p>
-									</div>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-md-12">
-									<h3 class="text-right">
-										h3. Lorem ipsum dolor sit amet.
-									</h3>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-md-3">
+							            <b-button variant="success">Buscar</b-button>
+						           </b-col>
+					        </b-row>
+                        
+			        <b-row>
+						<b-col md="12">
+							<b-row>
+								<b-col md="12">
+									   <b-jumbotron header="Titulo Noticias" lead="Link:www.páginawb.cl">
+										 <p>Resumen Noticias</p>
+										     <b-button variant="primary" href="#">Seguir leyendo</b-button>
+                                       </b-jumbotron>
+								</b-col>
+						    </b-row>
+							
+					<b-row>
+                    
+						<b-col md="12">
+                                    
+									 <h7 class="text-right">
+										  #Visitas=200
+									  </h7>
+						</b-col>
+					</b-row>
+		                <b-row>
+							<b-col md="3">
 									<div class="dropdown">
 										 
-										<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
-											Action
-										</button>
+										      <b-dropdown class="mx-1" right text="#EJE">
+                                              <b-dropdown-item>Eje 1</b-dropdown-item>
+                                              <b-dropdown-item>Eje 2</b-dropdown-item>
+                                              <b-dropdown-item>Eje 3</b-dropdown-item>
+                                              </b-dropdown>
+										   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+											 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+										    </div>
+									</div>
+							</b-col>
+								<b-col md="3">
+									<div class="dropdown">
+									       <b-dropdown class="mx-1" right text="#PAIS">
+                                           <b-dropdown-item>Pais 1 </b-dropdown-item>
+                                           <b-dropdown-item>Pais 2</b-dropdown-item>
+                                           <b-dropdown-item>Pais 3</b-dropdown-item>	
+                                            </b-dropdown> 
+									
+										    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+											  <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+										    </div>
+									</div>
+								</b-col>
+								<b-col md="3">
+									<div class="dropdown">
+										    <b-dropdown class="mx-1" right text="#ORGANISMO">
+                                            <b-dropdown-item>Organismo 1 </b-dropdown-item>
+                                            <b-dropdown-item>Organismo 2</b-dropdown-item>
+                                            <b-dropdown-item>Organismo 3</b-dropdown-item>	
+                                            </b-dropdown>
+							
+										   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+											 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+										  </div>
+									</div>
+								</b-col>
+								<b-col md="3">
+									<div class="dropdown">
+										 
+										   <b-dropdown class="mx-1" right text="#FECHA">
+                                           <b-dropdown-item>Fecha 1 </b-dropdown-item>
+                                           <b-dropdown-item>Fecha 2</b-dropdown-item>
+                                           <b-dropdown-item>Fecha 3</b-dropdown-item>	
+                                           </b-dropdown>
 										<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 											 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
 										</div>
 									</div>
-								</div>
-								<div class="col-md-3">
+								</b-col>
+						</b-row>
+							<b-row>
+								<b-col md="12">
+								</b-col>
+							</b-row>
+							<b-row>
+								<b-col md="3">
+									<h7>
+										BIBLIOGRAFIA
+									</h7>
+								</b-col>
+								<b-col md="3">
 									<div class="dropdown">
 										 
-										<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
-											Action
-										</button>
+										   <b-dropdown class="mx-1" right text="Nombre">
+                                           <b-dropdown-item>Nombre Bibliografia 1 </b-dropdown-item>
+                                           <b-dropdown-item>Nombre Bibliografia 2</b-dropdown-item>
+                                           <b-dropdown-item>Nombre Bibliografia 3</b-dropdown-item>	
+                                           </b-dropdown> 
+                                           
 										<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 											 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
 										</div>
-									</div>
-								</div>
-								<div class="col-md-3">
-									<div class="dropdown">
-										 
-										<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
-											Action
-										</button>
-										<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-											 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3">
-									<div class="dropdown">
-										 
-										<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
-											Action
-										</button>
-										<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-											 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-md-12">
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-md-3">
-									<h3>
-										h3. Lorem ipsum dolor sit amet.
-									</h3>
-								</div>
-								<div class="col-md-3">
-									<div class="dropdown">
-										 
-										<button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
-											Action
-										</button>
-										<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-											 <a class="dropdown-item disabled" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-3">
-								</div>
-								<div class="col-md-3">
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-md-6">
-								</div>
-								<div class="col-md-6">
-									 
-									<button type="button" class="btn btn-success">
-										Button
-									</button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-2">
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+									 </div>
+								</b-col>
+								    <b-col md="3">
+                                        <b-input-group size="sm" prepend="link" >
+                                            <b-form-input value=" " class="text-right"></b-form-input>
+                                           </b-input-group>
+								    </b-col>
+								         <b-col md="3">
+								         </b-col>
+							</b-row>
+							<b-row>
+								<b-col md="6">
+								</b-col>
+								     <b-col md="6">
+									    <b-button variant="success">Editar</b-button>
+								     </b-col>
+							</b-row>
+						
+							  
+				     </b-col>
+			        </b-row>
+		            </b-col>
+				<b-col md="2">
+				</b-col>
+			</b-row>
+		</b-col>
+	</b-row>
+
+
+</b-container>
 </template>>
 <script>
 export default {
