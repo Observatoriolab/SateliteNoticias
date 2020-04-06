83f03b087715ba6566982a7081a980579dc61152
Observatorio - Satelite Noticias <obstechlab@gmail.com>
2020-04-06 12:36:57 -0400

    Cambio vue-select a vue-multiselect en ambos primarytags

--- src/miniComponents/edition/PrimaryTagsEdition.vue
+++ src/miniComponents/edition/PrimaryTagsEdition.vue
@@ -2,92 +2,154 @@
   <div
     style=" width: 90%; margin-left:auto; margin-right:auto; text-align:center;"
   >
-    <!-- PRIMARY TAGS EDITION COMPONENT -->
-    <b-row>
-      <b-col md="2">
-        <h6>EJE</h6>
-        <b-dropdown
-          split
-          split-variant="outline-primary"
-          variant="primary"
-          text="#EJE"
-          class="m-2"
-        >
-          <b-dropdown-item href="#">Eje 1</b-dropdown-item>
-          <b-dropdown-item href="#">Eje 2</b-dropdown-item>
-          <b-dropdown-item href="#">Eje 3</b-dropdown-item>
-        </b-dropdown>
-      </b-col>
+      <h4 style="color: blue">Tags primarios</h4>
 
-      <b-col md="3">
-        <h6>PAIS/REGION</h6>
-        <b-dropdown
-          split
-          split-variant="outline-primary"
-          variant="primary"
-          text="#PAIS"
-          class="m-2"
-        >
-          <b-dropdown-item href="#">Pais 1</b-dropdown-item>
-          <b-dropdown-item href="#">Pais 2</b-dropdown-item>
-          <b-dropdown-item href="#">Pais 3</b-dropdown-item>
-        </b-dropdown>
+    <b-row>
+      <b-col md="4">
+        <h6>EJE</h6>
+       <multiselect  :taggable="true" @tag="addTag" v-model="value" :selectLabel="''" :options="options" :placeholder="value" label="name" track-by="name"></multiselect>
+
       </b-col>
 
       <b-col md="4">
-        <h6>ORGANISMO/ENTIDAD</h6>
 
-        <b-dropdown
-          split
-          split-variant="outline-primary"
-          variant="primary"
-          text="#ORGANISMO"
-          class="m-2"
-        >
-          <b-dropdown-item href="#">Organismo 1</b-dropdown-item>
-          <b-dropdown-item href="#">Organismo 2</b-dropdown-item>
-          <b-dropdown-item href="#">Organismo 3</b-dropdown-item>
-        </b-dropdown>
+
+        <h6>PAIS/REGION</h6>
+                <multiselect :taggable="true" @tag="addTag2"  v-model="value2" :selectLabel="''" :options="options2" placeholder="Selecciona uno" label="name" track-by="name"></multiselect>
+
+
       </b-col>
 
-      <b-col md="3">
-        <h6>FECHA</h6>
-        <b-dropdown
-          split
-          split-variant="outline-primary"
-          variant="primary"
-          text="#FECHA"
-          class="m-2"
-        >
-          <b-dropdown-item href="#">Fecha 1</b-dropdown-item>
-          <b-dropdown-item href="#">Fecha 2</b-dropdown-item>
-          <b-dropdown-item href="#">Fecha 3</b-dropdown-item>
-        </b-dropdown>
+      <b-col md="4">
+        <h6>ORGANISMO</h6>
+            <multiselect  :taggable="true" @tag="addTag3" v-model="value3" :selectLabel="''" :options="options3" placeholder="Selecciona uno" label="name" track-by="name"></multiselect>
+
       </b-col>
+
     </b-row>
-
-    <!-- PRIMARY TAGS EDITION COMPONENT -->
   </div>
 </template>
 <script>
+import Multiselect from 'vue-multiselect'
+//TODO: SE TIENE QUE AGREGAR LA FECHA
 export default {
   name: "PrimaryTagsEdition",
   props: {
-    tags: Array
+    tags: Array,
+    tags2: Array,
+    tags3: Array
+  },
+  components: {
+    Multiselect
   },
 
   data: () => ({
-    tagsInternal: null
+    tagsInternal: null,
+    tagsInternal2: null,
+    tagsInternal3:null,
+    bibliographyNameInternal:'',
+    bibliographyNameInternal2:'',
+    bibliographyNameInternal3:'',
+    value: { name: '', code: ''},
+    options: [
+
+    ],
+     value2: { name: '', code: ''},
+    options2: [
+
+    ],
+     value3: { name: '', code: ''},
+    options3: [
+
+    ],
   }),
 
   methods: {
-    onChange() {
-      this.$emit("primary-tags-change", this.tagsInternal);
-    }
+     addPrimaryTags() {
+      this.createStrings()
+      this.$emit(
+        "primarytags-change",
+        this.bibliographyNameInternal,
+        this.bibliographyNameInternal2,
+        this.bibliographyNameInternal3
+      );
+    },
+     createArrays() {
+        var nameArray = this.tagsInternal.split(";");
+        var nameArray2 = this.tagsInternal2.split(";");
+        var nameArray3 = this.tagsInternal3.split(";");
+ 
+        for (var i = 0; i < nameArray.length - 1; i++) {
+          this.$set(this.options, i, {name:nameArray[i], code:nameArray[i]});
+        }
+
+        for (i = 0; i < nameArray2.length - 1; i++) {
+          this.$set(this.options2, i, {name:nameArray[i], code:nameArray[i]});
+        }
+
+        for (i = 0; i < nameArray3.length - 1; i++) {
+          this.$set(this.options3, i, {name:nameArray[i], code:nameArray[i]});
+        }
+    },
+    createStrings() {
+      var i;
+      for ( i = 0; i < this.options.length; i++) {
+        this.bibliographyNameInternal =
+          this.bibliographyNameInternal + this.options[i].name + ";";
+      }
+      for (i = 0; i < this.options2.length; i++) {
+        this.bibliographyNameInternal2 =
+          this.bibliographyNameInternal2 + this.options2[i].name + ";";
+      }
+      for ( i = 0; i < this.options3.length; i++) {
+        this.bibliographyNameInternal3 =
+          this.bibliographyNameInternal3 + this.options3[i].name + ";";
+      }
+    },
+      addTag (newTag) {
+        const tag = {
+          name: newTag,
+          code: newTag
+        }
+        console.log('Este es el nuevo tag')
+        console.log(newTag)
+        console.log(tag)
+
+        this.options.push(tag)
+
+    },
+        addTag2 (newTag) {
+        const tag = {
+          name: newTag,
+          code: newTag
+        }
+        console.log('Este es el nuevo tag')
+        console.log(newTag)
+        console.log(tag)
+
+        this.options2.push(tag)
+
+    },
+        addTag3 (newTag) {
+        const tag = {
+          name: newTag,
+          code: newTag
+        }
+        console.log('Este es el nuevo tag')
+        console.log(newTag)
+        console.log(tag)
+
+        this.options3.push(tag)
+
+    },
   },
 
   created() {
     this.tagsInternal = this.tags;
+    this.tagsInternal2 = this.tags2;
+    this.tagsInternal3 = this.tags3;
+    //this.createArrays()
+
   }
 };
 </script>
--- src/miniComponents/news/PrimaryTags.vue
+++ src/miniComponents/news/PrimaryTags.vue
@@ -3,88 +3,104 @@
 
   <div style="width: 96%;  margin-left:auto;margin-right:auto">
     <b-row style="color: blue;border: 1px solid blue; text-align:center;" fluid>
-      <b-col md="3">
-        <div style="padding: 5px"></div>
-
+      <b-col md="4">
         <h6>EJE</h6>
-        <b-dropdown
-          split
-          split-variant="outline-primary"
-          variant="primary"
-          text="#EJE"
-          class="m-2"
-        >
-          <b-dropdown-item href="#">Eje 1</b-dropdown-item>
-          <b-dropdown-item href="#">Eje 2</b-dropdown-item>
-          <b-dropdown-item href="#">Eje 3</b-dropdown-item>
-        </b-dropdown>
+       <multiselect  v-model="value" :selectLabel="''"  placeholder="value" label="name" track-by="name"></multiselect>
+
       </b-col>
 
-      <b-col md="3">
-        <div style="padding: 5px"></div>
+      <b-col md="4">
+
 
         <h6>PAIS/REGION</h6>
-        <b-dropdown
-          split
-          split-variant="outline-primary"
-          variant="primary"
-          text="#PAIS"
-          class="m-2"
-        >
-          <b-dropdown-item href="#">Pais 1</b-dropdown-item>
-          <b-dropdown-item href="#">Pais 2</b-dropdown-item>
-          <b-dropdown-item href="#">Pais 3</b-dropdown-item>
-        </b-dropdown>
+                <multiselect    v-model="value2" :selectLabel="''"  placeholder="Selecciona uno" label="name" track-by="name"></multiselect>
+
+
       </b-col>
 
-      <b-col md="3">
-        <div style="padding: 5px"></div>
+      <b-col md="4">
+        <h6>ORGANISMO</h6>
+            <multiselect   v-model="value3" :selectLabel="''"  placeholder="Selecciona uno" label="name" track-by="name"></multiselect>
 
-        <h6>ORGANISMO/ENTIDAD</h6>
-
-        <b-dropdown
-          split
-          split-variant="outline-primary"
-          variant="primary"
-          text="#ORGANISMO"
-          class="m-2"
-        >
-          <b-dropdown-item href="#">Organismo 1</b-dropdown-item>
-          <b-dropdown-item href="#">Organismo 2</b-dropdown-item>
-          <b-dropdown-item href="#">Organismo 3</b-dropdown-item>
-        </b-dropdown>
       </b-col>
 
-      <b-col md="3">
-        <div style="padding: 5px"></div>
-
-        <h6>FECHA</h6>
-        <b-dropdown
-          split
-          split-variant="outline-primary"
-          variant="primary"
-          text="#FECHA"
-          class="m-2"
-        >
-          <b-dropdown-item href="#">Fecha 1</b-dropdown-item>
-          <b-dropdown-item href="#">Fecha 2</b-dropdown-item>
-          <b-dropdown-item href="#">Fecha 3</b-dropdown-item>
-        </b-dropdown>
-      </b-col>
     </b-row>
   </div>
 
   <!-- PRIMARY TAGS COMPONENT -->
 </template>
 <script>
+import Multiselect from 'vue-multiselect'
+
 export default {
   name: "PrimaryTags",
   props: {
-    primaryTags: Array
+    first: String,
+    second: String,
+    third: String,
+    tags: Array,
+    tags2: Array,
+    tags3: Array
   },
-  components: {},
+  components: {
+    Multiselect
+  },
+
   data() {
-    return {};
+    return {
+    value: { name: '', code: ''},
+    options: [
+
+    ],
+     value2: { name: '', code: ''},
+    options2: [
+
+    ],
+     value3: { name: '', code: ''},
+    options3: [
+
+    ],
+
+
+    };
+  },
+  methods: {
+  
+    createArrays() {
+      var nameArray = this.tagsInternal.split(";");
+      var nameArray2 = this.tagsInternal2.split(";");
+      var nameArray3 = this.tagsInternal3.split(";");
+
+      for (var i = 0; i < nameArray.length - 1; i++) {
+        this.$set(this.options, i, {name:nameArray[i], code:nameArray[i]});
+      }
+
+      for (i = 0; i < nameArray2.length - 1; i++) {
+        this.$set(this.options2, i, {name:nameArray[i], code:nameArray[i]});
+      }
+
+      for (i = 0; i < nameArray3.length - 1; i++) {
+        this.$set(this.options3, i, {name:nameArray[i], code:nameArray[i]});
+      }
+     },
+   
+  },
+
+  created() {
+    this.value.name=this.first
+    this.value.code=this.first
+
+    this.value.name=this.second
+    this.value.code=this.second
+
+    this.value.name=this.third
+    this.value.code=this.third
+/*  this.tagsInternal = this.tags;
+    this.tagsInternal2 = this.tags2;
+    this.tagsInternal3 = this.tags3;
+  */
+    //this.createArrays()
+
   }
 };
 </script>
